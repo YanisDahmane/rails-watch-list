b3c05a0607e3b0611ef4d9b673368626050ab618
Yanis Dahmane <yanis.dahmane.pro@gmail.com>
2022-11-17 16:25:48 +0100

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Movie.delete_all
+List.delete_all
+Bookmark.delete_all
+puts "Database cleaned!"
+puts "Creating movies..."
+m1 = Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9).id
+m2 = Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7).id
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "Movies created!"
+puts "Creating list..."
+l1 = List.create(name: "Test").id
+l2 = List.create(name: "Prout").id
+puts "Lists created!"
+puts "Creating bookmark"
+Bookmark.create(comment: "Book1dfsdfsfddfs", list_id: l1, movie_id: m1)
+Bookmark.create(comment: "Book2fdsfdsdf", list_id: l2, movie_id: m2)
+puts "Book created!"
